f9e5cd195d839b1371cc46e33ca316d544b772fa
MubashirJ <mubashirmjjawed@gmail.com>
2024-10-08 14:32:54 +0500

    issues resolved

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import SubHero from "@/components/SubHero";
 // import TechStack from "@/components/TechStack";
-import Image from "next/image";
+// import Image from "next/image";
 
 export default function Home() {
   return (
